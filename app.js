f4d4384616918c3c6730870b74b8cd14be0afec4
aldhipradana <aldhipradana@gmail.com>
2022-04-22 17:22:10 +0800

    adding cors function

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
+app.use(
+    cors({
+        origin: "*",
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
+    })
+)
+
 
 const db = require('./app/models');
 
--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,5 @@
 require('dotenv/config')
 
 module.exports = {
-    url: process.env.MONGODB_URL
+    url: process.env.ATLAS_URL
 }
